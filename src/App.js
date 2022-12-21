0b016326b303f0c081b20a09a760c871caf97bca
ptrpengdev <69882989+ptrpengdev@users.noreply.github.com>
2022-12-21 11:06:17 -0500

    update version 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Shengwei Updated.
         </a>
       </header>
     </div>
